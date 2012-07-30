423e6c7070703a971e17caeb8fb9234e99b7225d
Xalicus <Xalicus@fullsail.edu>
2012-07-30 09:26:36 -0700

    First Commit.

---
+++ js/json.js
@@ -0,0 +1,209 @@
+/*
+Author: Kevin Ward
+Class: MIU1208
+Name: Project 1
+Date: 07-30-2012
+*/
+
+var json = {
+	"pet1": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet2": {
+		"petGroups": ["Kool Pet Type:", "Cats"],
+		"petName": ["Kool Pet\'s Name:", "Katty"],
+		"petEmail": ["Kool Pet Email:", "Katty@petmail.com"],
+		"genderValue": ["Gender:", "Female"],
+		"favePet": ["Favorite Kool Pet:", "No"],
+		"birthDate": ["Date of Birth:", "2012-01-01"],
+		"koolness": ["Koolness Factor:", "25"],
+		"comments": ["Comments:", "She's so cute!"]
+	},
+	"pet3": {
+		"petGroups": ["Kool Pet Type:", "Mythical"],
+		"petName": ["Kool Pet\'s Name:", "Drako"],
+		"petEmail": ["Kool Pet Email:", "Drako@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "0001-01-01"],
+		"koolness": ["Koolness Factor:", "100"],
+		"comments": ["Comments:", "No, I do not taste good with Ketchup!"]
+	},
+	"pet4": {
+		"petGroups": ["Kool Pet Type:", "Rodents"],
+		"petName": ["Kool Pet\'s Name:", "Ratigan"],
+		"petEmail": ["Kool Pet Email:", "Ratigan@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "No"],
+		"birthDate": ["Date of Birth:", "2003-05-02"],
+		"koolness": ["Koolness Factor:", "33"],
+		"comments": ["Comments:", "I am the pumpkin king!"]
+	},
+	"pet5": {
+		"petGroups": ["Kool Pet Type:", "Reptiles"],
+		"petName": ["Kool Pet\'s Name:", "Jasmine"],
+		"petEmail": ["Kool Pet Email:", "Jasmine@petmail.com"],
+		"genderValue": ["Gender:", "Female"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2011-09-14"],
+		"koolness": ["Koolness Factor:", "45"],
+		"comments": ["Comments:", "She looks like a dinosaur!"]
+	},
+	"pet6": {
+		"petGroups": ["Kool Pet Type:", "Birds"],
+		"petName": ["Kool Pet\'s Name:", "Flappy McFlappins"],
+		"petEmail": ["Kool Pet Email:", "Flaptastick@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "No"],
+		"birthDate": ["Date of Birth:", "2099-12-01"],
+		"koolness": ["Koolness Factor:", "10"],
+		"comments": ["Comments:", "He's Flappy McFlappins, at your service."]
+	},
+	"pet7": {
+		"petGroups": ["Kool Pet Type:", "Farm_Animals"],
+		"petName": ["Kool Pet\'s Name:", "Bessie"],
+		"petEmail": ["Kool Pet Email:", "Bessie@petmail.com"],
+		"genderValue": ["Gender:", "Female"],
+		"favePet": ["Favorite Kool Pet:", "No"],
+		"birthDate": ["Date of Birth:", "2012-07-07"],
+		"koolness": ["Koolness Factor:", "01"],
+		"comments": ["Comments:", "She's a cow, Moo."]
+	},
+	"pet8": {
+		"petGroups": ["Kool Pet Type:", "Mythical"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet9": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet10": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet11": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet12": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet13": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet14": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet15": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet16": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet17": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet18": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet19": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet20": {
+		"petGroups": ["Kool Pet Type:", "Dogs"],
+		"petName": ["Kool Pet\'s Name:", "Snoopy"],
+		"petEmail": ["Kool Pet Email:", "Snoopy@petmail.com"],
+		"genderValue": ["Gender:", "Male"],
+		"favePet": ["Favorite Kool Pet:", "Yes"],
+		"birthDate": ["Date of Birth:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "50"],
+		"comments": ["Comments:", "He's so cute!"]
+	}
+};
